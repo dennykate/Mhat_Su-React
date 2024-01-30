ea757790c639fe0fdec2089daf740331bb23a32a
Denny_Kate <dennykate22@gmail.com>
2024-01-30 20:12:47 +0630

    feat: google auth button

---
+++ src/features/login/components/GoogleAuthButton.jsx
@@ -0,0 +1,23 @@
+import useMutate from "@/hooks/useMuate";
+import { useGoogleLogin } from "@react-oauth/google";
+
+const GoogleAuthButton = ({ setToken }) => {
+  const [onSubmit] = useMutate({
+    navigateBack: false,
+    callback: (data) => {
+      setToken(data?.access_token);
+    },
+  });
+
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      onSubmit("/auth/google", {
+        access_token: tokenResponse?.access_token,
+      });
+    },
+  });
+
+  return <button onClick={login}>Google Login</button>;
+};
+
+export default GoogleAuthButton;
